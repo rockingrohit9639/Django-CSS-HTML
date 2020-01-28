d2e5aaa239e9c44ebf0ec712db2408bd287347d7
Cassandra Talbot <40823509+CassandraTalbot32@users.noreply.github.com>
2020-01-28 14:39:07 +0000

    Add files via upload

---
+++ models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+	title		= models.CharField(max_length=120)
+	description = models.TextField(blank=True, null=True)
+	price		= models.DecimalField(decimal_places=2, max_digits=10)
+	summary		= models.TextField()
+	date		= models.DateField(null=True)
+	contact		= models.EmailField(max_length=254)
+	featured	= models.NullBooleanField()
+	
